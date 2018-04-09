090e1e6f96d730989b2ae4f0230033f11d3a994e
Kimberly Zhang <kyz@andrew.cmu.edu>
2018-04-09 02:19:59 -0400

    data cleaning

--- recommend.py
+++ recommend.py
@@ -1,5 +1,28 @@
+# import the necessary modules
 import pandas as pd
+import numpy as np
+import copy 
+import string
+from ast import literal_eval
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+# read the data files
 credits= pd.read_csv('./the-movies-dataset/credits.csv', sep= ',')
-print(credits.head(5))
+keywords= pd.read_csv('./the-movies-dataset/keywords.csv', sep= ',')
+ratings= pd.read_csv('./the-movies-dataset/ratings.csv', sep= ',')
+metadata= pd.read_csv('./the-movies-dataset/movies_metadata.csv', sep= ',', \
+low_memory= False)
+
+###############################################################################
+# Data Cleaning
+###############################################################################
+# merge keywords, credits, and metadata for analysis
+nMetadata= copy.deepcopy(metadata)
+metadataLen= len(metadata['id'])
+for i in range(metadataLen):
+    for j in metadata['id'][i]:
+        if j not in string.digits:
+            nMetadata=nMetadata.drop(nMetadata.index[i])
+
+
+
